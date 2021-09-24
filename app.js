5863a3cb0a8154e1d689a5688c6b60e747d584a6
IHomov <innapariy@gmail.com>
2021-09-24 14:59:38 +0300

    active card
    created function for
    transmit data which slide will be active

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const slides = document.querySelectorAll('.slide')
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        
-        slide.classList.add('active')
-    })
+function activityPlagin (activePicture = 0) {
+    const slides = document.querySelectorAll('.slide')
+    
+        slides[activePicture].classList.add('active')
+    
+        for (const slide of slides) {
+            slide.addEventListener('click', () => {
+                clearActiveClasses()
+                
+                slide.classList.add('active')
+            })
 
+        }
+
+        function clearActiveClasses() {
+            slides.forEach((slide) => {
+                slide.classList.remove('active')
+            })
+        }
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+activityPlagin(4);
